24fe3b4b637ca53b3525ee5f5edbd479dc5d1675
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-01-29 15:02:27 +0200

    1

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import tkinter
 
 
-def main():
-    main_window = tkinter.Tk()
+class MyGUI:
+    def __init__(self):
+        self.main_window = tkinter.Tk()
 
-    tkinter.mainloop()
+        self.main_window.title('Мой первый GUI!')
+
+        self.label_1 = tkinter.Label(self.main_window, text='Привет мир!')
+        self.label_2 = tkinter.Label(self.main_window, text='Это моя программа!')
+
+        self.label_1.pack()
+        self.label_2.pack()
+
+        tkinter.mainloop()
+
 
 if __name__ == '__main__':
-    main()
+    my_gui = MyGUI()
