a5e819a8df4f84708d04ba1251810c46a30949e2
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-01-31 13:48:29 +0200

    1

--- main.py
+++ main.py
@@ -28,8 +28,47 @@ def test1():
 
 
 def test2():
-    pass
+    class KiloConverterGUI:
+        def __init__(self):
+            self.main_window = tkinter.Tk()
+
+            self.top_frame = tkinter.Frame(self.main_window)
+            self.bottom_frame = tkinter.Frame(self.main_window)
+
+            self.prompt_label = tkinter.Label(self.top_frame,
+                                              text='Введите растояние в километрах: ')
+            self.kilo_entry = tkinter.Entry(self.top_frame,
+                                            width=10)
+
+            self.prompt_label.pack(side='left')
+            self.kilo_entry.pack(side='left')
+
+            self.calc_button = tkinter.Button(self.bottom_frame, text='Переобразовать',
+                                              command=self.convert)
+            self.quit_button = tkinter.Button(self.bottom_frame,
+                                              text='Выйти',
+                                              command=self.main_window.destroy)
+
+            self.calc_button.pack(side='left')
+            self.quit_button.pack(side='left')
+
+            self.top_frame.pack()
+            self.bottom_frame.pack()
+
+            tkinter.mainloop()
+
+        def convert(self):
+            kilo = float(self.kilo_entry.get())
+
+            miles = kilo * 0.6214
+
+            tkinter.messagebox.showinfo('Результаты',
+                                        str(kilo)+
+                                        ' километров эквивалентно ' +
+                                        str(miles) + ' милям.')
+
+    kilo_conv = KiloConverterGUI()
 
 
 if __name__ == '__main__':
-    pass
+    test2()
